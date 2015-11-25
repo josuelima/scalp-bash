4b93055500e5442fb845e1a732cf4572a23975a6
Josue Lima <josuedsi@gmail.com>
2015-11-25 09:20:35 -0500

    Add log path to var

--- run-scalp.sh
+++ run-scalp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 START=`date +%d/%b/%Y:%H:00:00`
 FINISH=`date +%d/%b/%Y:%H:59:59`
-python scalp.py -l access-http.log -f ./filter.xml -p "$START;$FINISH" -o ./logs --text
+
+# Set the apachje log folder
+APACHE_LOG=
+
+python scalp.py -l $APACHE_LOG -f ./filter.xml -p "$START;$FINISH" -o ./logs --text
